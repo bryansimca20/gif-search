c3f4a1b6c60094fafb156429cdcc512368388545
Bryan Simca <bryansimca20@gmail.com>
2020-10-06 19:24:38 -0700

    Fixed minor issues

--- src/App.js
+++ src/App.js
@@ -9,12 +9,26 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     }
   }
 
   componentDidMount() {
     axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => {
+        this.setState({
+          gifs: response.data.data,
+          loading: false
+        })
+      })
+      .catch(err => {
+        console.log('Error fetching and parsing data' + err)
+      })
+  }
+
+  performSearch = (query) => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=32&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
           gifs: response.data.data
@@ -32,13 +46,19 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />
+            <SearchForm
+              onSearch = {this.performSearch}
+            />
           </div>
         </div>
         <div className="main-content">
-          <GifList
-            data = {this.state.gifs}
-          />
+          {
+            (this.state.loading)
+            ? <p> Loading ... </p>
+            : <GifList
+               data = {this.state.gifs}
+              />
+          }
         </div>
       </div>
     );
