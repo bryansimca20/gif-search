0b386eea4196ba1981d5acb17b8b3d9e9a398ee7
Bryan Simca <bryansimca20@gmail.com>
2020-10-08 11:40:51 -0700

    Updates

---
+++ precache-manifest.e31ddeff7e2fa1fb7c6b58c439811286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7f2ea27772af187ce3b3574ecfac4f",
+    "url": "/gif-search/index.html"
+  },
+  {
+    "revision": "10bc2ade2587fb9df7bb",
+    "url": "/gif-search/static/css/main.7a0e6566.chunk.css"
+  },
+  {
+    "revision": "ea6ed40f2db1cdba23b0",
+    "url": "/gif-search/static/js/2.c10ad0dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gif-search/static/js/2.c10ad0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bc2ade2587fb9df7bb",
+    "url": "/gif-search/static/js/main.c645c9b4.chunk.js"
+  },
+  {
+    "revision": "64b78f528396f1316ec4",
+    "url": "/gif-search/static/js/runtime-main.bc74187c.js"
+  }
+]);
